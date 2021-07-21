9dd0dc84cf024ce4629577671a027db58247f279
harrygcoppock <harrygcoppock@gmail.com>
2021-07-21 10:30:41 +0100

    call gpu modified for me

--- call_gpu.py
+++ call_gpu.py
@@ -57,7 +57,7 @@ with open(description_file, 'w') as ds:
 #Make submission file
 
 job_file = os.path.join(input_path, f"{case_number}.sh")
-file_to_run = 'wandb agent --count 1 mosquito/Moskeet-Endtoendapproach/oa2w7ys8'#'python /vol/bitbucket/hgc19/mosquito/Moskeet/Endtoendapproach/main.py' # swap main.py with your script
+file_to_run = 'wandb agent --count 1 mosquito/Moskeet-Endtoendapproach_Endtoendapproach/41cso9g9'#'python /vol/bitbucket/hgc19/mosquito/Moskeet/Endtoendapproach/main.py' # swap main.py with your script
 with open(job_file, 'w') as fh:
 
     fh.writelines("#!/bin/bash\n")
